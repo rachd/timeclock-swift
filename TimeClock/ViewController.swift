a46b031f0799bc7b4fba0b33a1e1d7cda1380ad4
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-07-29 22:21:04 -0400

    initial commit

---
+++ TimeClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeClock
+//
+//  Created by Rachel Dorn on 7/29/16.
+//  Copyright © 2016 RachelDorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
